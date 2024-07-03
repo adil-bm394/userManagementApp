b088f326cbb25b82d55cff72d8ce93cb5a4a1231
adil-bm394 <adil@binmile.com>
2024-07-03 16:25:57 +0530

    updated,so that only one admin can register

--- src/Hooks/useRegister.ts
+++ src/Hooks/useRegister.ts
@@ -13,11 +13,18 @@ export const useRegister = () => {
 
     try {
       const users = await getAllUsersFromDB();
-      if (users.length >= 6) {
+      if (users.length >= 6) {        //One user as a Admin and 5 users
         setError("5 Users already registered");
         console.log("5 Users already registered");
         return;
       }
+      const adminExists = users.some(user => user.role === 'admin');
+      if (newUser.role === 'admin' && adminExists) {
+        setError('One admin already exists');
+        console.log('One admin already exists');
+        return;
+      }
+      
       const id = (users.length + 1).toString();
       const user = { ...newUser, id };
       await addUserToDB(user);
